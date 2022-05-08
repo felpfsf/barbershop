76b711ec0203d29bd8f29250022d7a623a155c0d
felpfsf <feferreira.sf@gmail.com>
2022-05-07 21:06:55 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll', onScroll)
+
+onscroll()
+
+function onScroll() {
+  showNavOnScroll()
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
